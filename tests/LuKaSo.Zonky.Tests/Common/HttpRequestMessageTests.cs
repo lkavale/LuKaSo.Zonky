bcb2dacc4dd400aeb81e97973464483050667675
lkavale <l.kavale@gmail.com>
2019-09-01 11:12:22 +0200

    Added requests tests
    ZonkyHttpRequests test added

---
+++ tests/LuKaSo.Zonky.Tests/Common/HttpRequestMessageTests.cs
@@ -0,0 +1,65 @@
+﻿using LuKaSo.Zonky.Common;
+using LuKaSo.Zonky.Models;
+using LuKaSo.Zonky.Models.Login;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Net.Http;
+using LuKaSo.Zonky.Extesions;
+
+namespace LuKaSo.Zonky.Tests.Common
+{
+    [TestClass]
+    public class HttpRequestMessageTests
+    {
+        private ZonkyHttpRequestMessage _request;
+        private readonly Uri _address = new Uri("http://www.zonky.cz/test");
+
+        [TestInitialize]
+        public void Init()
+        {
+            _request = new ZonkyHttpRequestMessage(HttpMethod.Get, _address);
+        }
+
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.AreEqual(HttpMethod.Get, _request.Method);
+            Assert.AreEqual(_address, _request.RequestUri);
+            Assert.AreEqual("application/json", _request.Headers.GetValues("Accept").First());
+        }
+
+        [TestMethod]
+        public void AddRequestPaging()
+        {
+            var page = 10;
+            var pageSize = 100;
+
+            _request.AddRequestPaging(page, pageSize);
+
+            Assert.AreEqual(page, int.Parse(_request.Headers.GetValues("x-page").First()));
+            Assert.AreEqual(pageSize, int.Parse(_request.Headers.GetValues("x-size").First()));
+        }
+
+        [TestMethod]
+        public void AddRequestAuthorization()
+        {
+            var token = new AuthorizationToken() { AccessToken = Guid.NewGuid() };
+
+            _request.AddRequestAuthorization(token);
+
+            Assert.AreEqual($"Bearer {token.AccessToken.ToString()}", _request.Headers.GetValues("Authorization").First());
+        }
+
+        [TestMethod]
+        public void AddRequestFilter()
+        {
+            var filter = new FilterOptions();
+            filter.Add("x", "10");
+
+            _request.AddRequestFilter(filter);
+
+            Assert.AreEqual(_address.AppendFilterOptions(filter), _request.RequestUri);
+        }
+    }
+}
---
+++ tests/LuKaSo.Zonky.Tests/Production/Long/BusinessCodeTests.cs
@@ -0,0 +1,29 @@
+﻿using LuKaSo.Zonky.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LuKaSo.Zonky.Tests.Production.Long
+{
+    [TestClass]
+    public class BusinessCodeTests
+    {
+        private ZonkyClient _zonkyClient;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var zonkyLogin = new SecretsJsonReader().Read().LoginOk;
+            _zonkyClient = new ZonkyClient(zonkyLogin, false);
+        }
+
+        //[TestMethod]
+        public void AllInvestorEvents()
+        {
+            var dic = _zonkyClient.GetAllInvestmentsAsync()
+                .GetAwaiter()
+                .GetResult()
+                .Select(i => new { Id = i.Id, Events = _zonkyClient.GetInvestmentEventsAsync(i.Id).GetAwaiter().GetResult() })
+                .ToDictionary(i => i.Id, i => i.Events);
+        }
+    }
+}
