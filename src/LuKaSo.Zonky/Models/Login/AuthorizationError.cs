0b5777ef24c5c13d71cb17a6ac111576f2441418
lkavale <l.kavale@gmail.com>
2019-09-11 21:08:11 +0200

    Added authorization errors
    Added authorization errors

---
+++ src/LuKaSo.Zonky/Models/Login/AuthorizationError.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace LuKaSo.Zonky.Models.Login
+{
+    public class AuthorizationError
+    {
+        [JsonProperty("error")]
+        public string Error { get; set; }
+
+        [JsonProperty("error_description")]
+        public string Description { get; set; }
+
+        [JsonProperty("error_code")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public AuthorizationErrorCode Code { get; set; }
+
+        [JsonProperty("mfa_token")]
+        public Guid? MfaToken { get; set; }
+    }
+}
---
+++ src/LuKaSo.Zonky/Models/Login/AuthorizationErrorCode.cs
@@ -0,0 +1,8 @@
+﻿namespace LuKaSo.Zonky.Models.Login
+{
+    public enum AuthorizationErrorCode
+    {
+        MFA_REQUIRED,
+        CAPTCHA_REQUIRED
+    }
+}
