9efd0cedfb6fb2b14092046d2947def98fc4c6a1
lkavale <l.kavale@gmail.com>
2019-09-11 21:07:38 +0200

    Added MFA exception
    Added MFA exception

--- src/LuKaSo.Zonky/Exceptions/BadResponceException.cs
+++ src/LuKaSo.Zonky/Exceptions/BadResponceException.cs
@@ -7,6 +7,9 @@ namespace LuKaSo.Zonky.Exceptions
     [Serializable]
     public class BadResponceException : Exception
     {
+        public BadResponceException(HttpResponseMessage message) : base($"Unexpected server responce retrieved. \r\n {message.ToString()} \r\n with content {message.Content.ReadAsStringAsync().GetAwaiter().GetResult()}")
+        { }
+
         public BadResponceException(HttpResponseMessage message, Exception ex) : base($"Unexpected server responce retrieved. \r\n {message.ToString()} \r\n with content {message.Content.ReadAsStringAsync().GetAwaiter().GetResult()}", ex)
         { }
 
---
+++ src/LuKaSo.Zonky/Exceptions/MultiFactorAuthenticationRequiredException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace LuKaSo.Zonky.Exceptions
+{
+    [Serializable]
+    public class MultiFactorAuthenticationRequiredException : Exception
+    {
+        public MultiFactorAuthenticationRequiredException()
+        { }
+
+        protected MultiFactorAuthenticationRequiredException(SerializationInfo info, StreamingContext context) : base(info, context)
+        { }
+    }
+}
